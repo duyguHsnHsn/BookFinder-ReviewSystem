2e15a3ac65fef7d828ccb49c84a55e1a047eb3de
Duygu Hasan <duyguhsnhsn@gmail.com>
2023-11-14 14:04:06 +0200

    initialise spring project

---
+++ book-recommendation-platform/src/main/java/com/fmi/bookrecommendationplatform/BookRecommendationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.bookrecommendationplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecommendationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRecommendationPlatformApplication.class, args);
+	}
+
+}
---
+++ book-recommendation-platform/src/test/java/com/fmi/bookrecommendationplatform/BookRecommendationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmi.bookrecommendationplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecommendationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
